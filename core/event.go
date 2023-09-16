3d4977d3403bfbc6799b517118f2801dd745e8d2
Kaan Yagci <kaan.yagci@makepad.fr>
2023-09-16 17:12:03 +0200

    feat(core): new fields added to the event structure
    done, owners and id fields added
    
    Signed-off-by: Kaan Yagci <kaan.yagci@makepad.fr>

--- core/event.go
+++ core/event.go
@@ -6,10 +6,13 @@ type User struct {
 }
 
 type Event struct {
+	ID          string      `json:"id"`          // The unique id of the event
 	Title       string      `json:"title"`       // Title of the event
 	Description string      `json:"description"` // The descroption of the event
 	Assignee    []User      `json:"assignee"`    // The list of user assigned to that event
 	SharedWith  []User      `json:"sharedWith"`  // Users that the event shared with
 	Dates       []time.Time `json:"dates"`       // The array of dates, it contains all history of dates, the last one will be the final date
 	Calendars   []string    `json:"calendars"`   // Calendar IDs that contains this event
+	Owners      []User      `json:"owners"`      // The owner of the event
+	Done        bool        `json:"done"`        // A boolean indicating that wether a task is done or not
 }
