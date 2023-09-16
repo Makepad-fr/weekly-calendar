7545ef680b83a5da8e18ce3bee3f33a6d1e7de19
Kaan Yagci <kaan.yagci@makepad.fr>
2023-09-16 17:13:40 +0200

    feat(core): add planning structure
    Signed-off-by: Kaan Yagci <kaan.yagci@makepad.fr>

---
+++ core/planning.go
@@ -0,0 +1,10 @@
+package core
+
+type Planning struct {
+	ID          string  `json:"id"`          // The unique ID of the calendar
+	Events      []Event `json:"events"`      // The list of event associated to that calendar
+	SharedWith  []User  `json:"sharedWith"`  // The list of users with who this event is shared
+	Owner       []User  `json:"owners"`      // The owner of the calendar
+	Title       string  `json:"title"`       // The title of the calendar
+	Description string  `json:"description"` // The description of the calendar
+}
